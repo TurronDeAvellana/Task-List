bae85eda675ace79adb5a6fa0f6984344e5400ac
? <Andresicokk@gmail.com>
2025-01-30 18:13:22 -0500

    add tasks
    now you can add tasks

--- script.js
+++ script.js
@@ -8,6 +8,38 @@ function Task(taskName, taskTime) {
 }
 
 
+
+function addTask(){
+
+    //select the container 
+    let targetDiv = document.getElementById("taskContainer");
+
+    //Get the values from the input fields
+    let taskName = document.getElementById("taskName").value;
+    let taskTime = document.getElementById("taskTime").value;
+
+    //Create a new task object
+    const newTask = new Task(taskName, taskTime);
+
+
+    let newInput = document.createElement("input");
+    newInput.type = "text";
+    newInput.value = newTask.taskName + " / " + newTask.taskTime;
+
+    //Append the input to the target div
+    targetDiv.appendChild(newInput);
+
+    //Clear the input fields
+    document.getElementById("taskName").value = "";
+    document.getElementById("taskTime").value = "";
+
+    document.getElementById("show").addEventListener("click", createInputField);
+
+}
+
+
+
+/*
 //This function takes the parameters from the input 
 function addTask() {
     let taskName = document.getElementById("taskName").value;
@@ -18,5 +50,4 @@ function addTask() {
     document.getElementById("showTasks").value = newTask.taskName + " / " + newTask.taskTime;
 }
 
-
-
+*/
