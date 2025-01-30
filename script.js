ddd1508e680acbe824ee60c9a81edd92bb65de12
? <Andresicokk@gmail.com>
2025-01-30 17:56:40 -0500

    Task
    Now it saves the tasks as objects

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 document.getElementById("saveTaskB").addEventListener("click", addTask);  
 
-function addTask() {
-    let taskName = parseFloat(document.getElementById("taskName").value);
-    let taskTime = parseFloat(document.getElementById("taskTime").value);
-    
-    sum = taskName + taskTime;
 
-    document.getElementById("show").value = sum;
+//Object with the task name and time:
+function Task(taskName, taskTime) {
+    this.taskName = taskName;
+    this.taskTime = taskTime;
+}
+
+
+//This function takes the parameters from the input 
+function addTask() {
+    let taskName = document.getElementById("taskName").value;
+    let taskTime = document.getElementById("taskTime").value;
+    
+    const newTask = new Task(taskName, taskTime);
+
+    document.getElementById("showTasks").value = newTask.taskName + " / " + newTask.taskTime;
 }
 
 
